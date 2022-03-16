8c360af096bbbf2c334c10a84c4644a64d348ffe
Henry Duong <duongminhhao23@gmail.com>
2022-03-16 10:13:04 +0700

    Add project files.

---
+++ Tuan4_DuongMinhHao/Controllers/GioHangController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan4_DuongMinhHao.Models;
+
+namespace Tuan4_DuongMinhHao.Controllers
+{
+    public class GioHangController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> listGiohang = Session["Giohang"] as List<Giohang>;
+            if(listGiohang == null)
+            {
+                listGiohang = new List<Giohang>();
+                Session["Giohang"] = listGiohang;
+            }
+            return listGiohang;
+        }
+        // GET: GioHang
+        public ActionResult ThemGioHang(int id, string strURL)
+        {
+            List<Giohang> ListGioHang = Laygiohang();
+            Giohang sanpham = ListGioHang.Find(n => n.masach == id);
+            if(sanpham == null)
+            {
+                sanpham = new Giohang(id);
+                ListGioHang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+        private int TongSoLuong()
+        {
+            int tsl = 0;
+            List<Giohang> listGioHang = Session["GioHang"] as List<Giohang>;
+            if(listGioHang != null)
+            {
+                tsl = listGioHang.Sum(n => n.iSoluong);
+            }
+            return tsl;
+        }
+
+        private int TongSoLuongSanPham()
+        {
+            int tsl = 0;
+            List<Giohang> listGioHang = Session["GioHang"] as List<Giohang>;
+            if(listGioHang != null)
+            {
+                tsl = listGioHang.Count;
+            }
+            return tsl;
+        }
+
+        private double TongTien()
+        {
+            double tt = 0;
+            List<Giohang> listGioHang = Session["GioHang"] as List<Giohang>;
+            if(listGioHang != null)
+            {
+                tt = listGioHang.Sum(n => n.dThanhtien);
+            }
+            return tt;
+        }
+
+        public ActionResult GioHang()
+        {
+            List<Giohang> ListGioHang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return View(ListGioHang);
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return PartialView();
+        }
+
+        public ActionResult XoaGioHang (int id)
+        {
+            List<Giohang> listGioHang = Laygiohang();
+            Giohang sanpham = listGioHang.SingleOrDefault(n => n.masach == id);
+            if (sanpham != null)
+            {
+                listGioHang.RemoveAll(n => n.masach == id);
+                return RedirectToAction("GioHang");
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult CapNhapGioHang(int id, FormCollection collection)
+        {
+            List<Giohang> listGioHang = Laygiohang();
+            Giohang sanpham = listGioHang.SingleOrDefault(n => n.masach == id);
+            if(sanpham != null)
+            {
+                sanpham.iSoluong = int.Parse(collection["txtSoLg"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<Giohang> listGioHang = Laygiohang();
+            listGioHang.Clear();
+            return RedirectToAction("GioHang");
+        }
+    }
+}
---
+++ Tuan4_DuongMinhHao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tuan4_DuongMinhHao.MvcApplication" Language="C#" %>
